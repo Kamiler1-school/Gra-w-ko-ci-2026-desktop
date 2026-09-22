805a62f5e50165e06f1bc9771797cf2f0fcdd5e5
Kamil <Kamil@ELRO-3>
2026-09-23 01:13:48 +0200

    Finalna wersja

--- Gra w kości 2026 desktop/MainWindow.xaml.cs
+++ Gra w kości 2026 desktop/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Gra_w_kości_2026_desktop;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,88 @@ using System.Windows.Shapes;
 
 namespace Gra_w_kości_2026_desktop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Kosc[] kosci;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            kosci = new Kosc[]
+            {
+                new Kosc(0),
+                new Kosc(0),
+                new Kosc(0),
+                new Kosc(0),
+                new Kosc(0)
+            };
+        }
+
+        private void RzutButton_Click(object sender, RoutedEventArgs e)
+        {
+            Kosc1Rzut(0, Kosc1Obraz);
+            Kosc1Rzut(1, Kosc2Obraz);
+            Kosc1Rzut(2, Kosc3Obraz);
+            Kosc1Rzut(3, Kosc4Obraz);
+            Kosc1Rzut(4, Kosc5Obraz);
+
+            int suma = 0;
+
+            for (int i = 0; i < kosci.Length; i++)
+            {
+                suma += kosci[i].LiczbaOczek;
+            }
+
+            WynikText.Text = suma.ToString();
+        }
+
+        private void Kosc1Rzut(int numer, Image obraz)
+        {
+            kosci[numer].RzutKoscia();
+
+            obraz.Source = new BitmapImage(
+                new Uri("/Image/"+kosci[numer].PlikiObrazow[kosci[numer].IdentyfikatorPliku],
+                UriKind.Relative));
+        }
+
+        private void ZmienDostepnosc(int numer, Image obraz)
+        {
+            if (kosci[numer].CzyDostepna)
+            {
+                kosci[numer].BlokujKosc();
+                obraz.Opacity = 0.5;
+            }
+            else
+            {
+                kosci[numer].OdblokujKosc();
+                obraz.Opacity = 1.0;
+            }
+        }
+
+        private void Kosc1_Click(object sender, MouseButtonEventArgs e)
+        {
+            ZmienDostepnosc(0, Kosc1Obraz);
+        }
+
+        private void Kosc2_Click(object sender, MouseButtonEventArgs e)
+        {
+            ZmienDostepnosc(1, Kosc2Obraz);
+        }
+
+        private void Kosc3_Click(object sender, MouseButtonEventArgs e)
+        {
+            ZmienDostepnosc(2, Kosc3Obraz);
+        }
+
+        private void Kosc4_Click(object sender, MouseButtonEventArgs e)
+        {
+            ZmienDostepnosc(3, Kosc4Obraz);
+        }
+
+        private void Kosc5_Click(object sender, MouseButtonEventArgs e)
+        {
+            ZmienDostepnosc(4, Kosc5Obraz);
         }
     }
 }
